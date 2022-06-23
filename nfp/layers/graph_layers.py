1a42ec99f654a1dd0109f2ed8a5a59bf6c18c123
SabariKumar <sabarinkumar@gmail.com>
2022-06-23 13:51:58 -0600

    Global feature updates to nfp

--- nfp/layers/graph_layers.py
+++ nfp/layers/graph_layers.py
@@ -70,7 +70,9 @@ class EdgeUpdate(GraphLayer):
   #      source_atom = self.gather([atom_state, connectivity[:, :, 0]])
   #      target_atom = self.gather([atom_state, connectivity[:, :, 1]])
         source_atom = self.gather([atom_state, connectivity[:, 0]])
+        source_atom = tf.tile(source_atom, tf.stack([1, bond_dims, 1]))
         target_atom = self.gather([atom_state, connectivity[:, 1]])
+        target_atom = tf.tile(target_atom, tf.stack([1, bond_dims, 1]))
 
         if not self.use_global:
             new_bond_state = self.concat(
